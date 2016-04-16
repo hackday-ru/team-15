9d4dc3144270d455c8866d710d39350566803d69
Dmitriy Baydin <baidin.d1993@gmail.com>
2016-04-16 18:33:50 +0300

    Models

--- app/models.py
+++ app/models.py
@@ -3,12 +3,16 @@ from app import db
 ROLE_USER = 0
 ROLE_ADMIN = 1
 
+
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    nickname = db.Column(db.String(64), unique = True)
-    email = db.Column(db.String(120), index = True, unique = True)
-    role = db.Column(db.SmallInteger, default = ROLE_USER)
-    posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
+    def __init__(self, id1, nickname, email):
+        self.id = id1
+        self.nickname = nickname
+        self.email = email
+
+    id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String(64), unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
 
     def is_authenticated(self):
         return True
@@ -23,13 +27,29 @@ class User(db.Model):
         return str(self.id)
 
     def __repr__(self):
-        return '<User %r>' % (self.nickname)    
-        
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+        return '<User %r>' % self.nickname
 
-    def __repr__(self):
-        return '<Post %r>' % (self.body)
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(140))
+    date = db.Column(db.DateTime)
+
+
+class Participant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('item.id'))
+    event_id = db.Column(db.Integer, db.ForeignKey('item.id'))
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(140))
+    cost = db.Column(db.Integer)
+    owner = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class Customers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    item_id = db.Column(db.Integer, db.ForeignKey('item.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
